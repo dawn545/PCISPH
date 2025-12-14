8610ad9f8984f2b9517a4da1b102716b319abdb2
dawn545 <2091109554@qq.com>
2025-12-15 02:24:57 +0800

    Translate comments in Particle.h to Chinese
    complete particle initialized

--- Particle.h
+++ Particle.h
@@ -5,16 +5,20 @@ using namespace Eigen;
 class Particle 
 {
     public:
-        Vector2d x;//position
-        Vector2d v;//velocity
-        Vector2d f;//force
-        Vector2d x_pred;
-        Vector2d v_pred;
-        Vector2d f_nonpressure;
+        Vector2d x;           // 当前位置
+        Vector2d v;           // 当前速度
+        Vector2d f;           // 压力力
+        Vector2d x_pred;      // 预测位置
+        Vector2d v_pred;      // 预测速度
+        Vector2d f_nonpressure; // 非压力力（重力+粘性）
 
-        float rho_pred;
-        float rho_error;
-        float rho;//density
-        float p;//pressure
-        Particle(float x_,float y_):x(x_,y_),v(0.f,0.f),f(0.f,0.f),rho(0.f),p(0.f){}
+        float rho;            // 当前密度
+        float rho_pred;       // 预测密度
+        float rho_error;      // 密度误差
+        float p;              // 压力
+        
+        Particle(float x_, float y_)
+            : x(x_, y_), v(0.f, 0.f), f(0.f, 0.f), 
+              x_pred(x_, y_), v_pred(0.f, 0.f),
+              rho(0.f), rho_pred(0.f), rho_error(0.f), p(0.f) {}
 };
